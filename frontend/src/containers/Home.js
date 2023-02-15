8c6b56e2cecacbb841764cd02d44b47e2e4aa267
austin25coding <acbarret@outlook.com>
2023-02-14 18:14:18 -0600

    splash w/ cards

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,9 +1,40 @@
-import Cards from '../components/Cards'
-
+import AusHCards from '../components/AusHCards'
+import wineimage from '../Images/winehome.jpg'
+import vineyardsimage from '../Images/vineyardshome.jpg'
+import regionsimage from '../Images/regionshome.avif'
+import homeimage from '../Images/home.avif'
 const Home = () => {
     return (
         <div>
-            <Cards />
+            <img src={homeimage} class="img-fluid" alt="..."></img>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <AusHCards
+                            title="Wines"
+                            text="Find wines that satisfy your taste"
+                            link="/Wines"
+                            image={wineimage}
+                        />
+                    </div>
+                    <div class="col">
+                        <AusHCards
+                            title="Vineyards"
+                            text="Discover amazing vineyards"
+                            link="/Vineyards"
+                            image={vineyardsimage}
+                        />
+                    </div>
+                    <div class="col">
+                        <AusHCards
+                            title="Regions"
+                            text="Explore new regions"
+                            link="/Vineyards"
+                            image={regionsimage}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
