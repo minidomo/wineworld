93d5b9626b81f9ac2f83a95f40e962cb9b1bacf0
austin25coding <acbarret@outlook.com>
2023-02-22 00:42:00 -0600

    fixed main title over image

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -2,15 +2,12 @@ import HomeCard from '../components/HomeCard'
 import wineimage from '../assets/w_home.jpg'
 import vineyardsimage from '../assets/v_home.jpg'
 import regionsimage from '../assets/r_home.jpg'
-import homeimage from '../assets/home.jpg'
+import homeimage from '../assets/home_logo.jpg'
 
 const Home = () => {
     return (
         <div>
             <img src={homeimage} class="img-fluid w-100" alt="..."></img>
-            <h1 class="display-1">
-                <p class="carousel-caption">WineWorld</p>
-            </h1>
             <div class="container text-center">
                 <div class="row">
                     <div class="col">
