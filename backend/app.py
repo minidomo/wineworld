23e79058904df83d19d384f7535dacf146fcb443
JB Ladera <domobaby10@gmail.com>
2023-02-23 20:02:26 -0600

    init setup for vscode and docker development

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world() :
-    return '''
+
+@app.route("/")
+def hello_world():
+    return """
     <h1> Welcome to the WineWorld API </h1> <br>
     Check out the <a href="https://documenter.getpostman.com/view/21507814/2s93CEvGRv">documentation</a>
-    '''
+    """
 
 
-if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = 4000, debug = True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4000, debug=True)
