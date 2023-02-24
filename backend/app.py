d12435078d5a6922f66bdb967e80d38c0df951bc
JB Ladera <domobaby10@gmail.com>
2023-02-23 23:21:01 -0600

    added https support

--- backend/app.py
+++ backend/app.py
@@ -12,4 +12,5 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4000, debug=True)
+    context = ("server.cert", "server.key")  # run `make sslgen` to generate the certificate
+    app.run(host="0.0.0.0", port=4000, debug=True, ssl_context=context)
