9227034b0df3f8b24a6d6ff77a0617cd6c7be939
JB Ladera <domobaby10@gmail.com>
2023-03-09 12:03:19 -0600

    fix environment check

--- frontend/tests/driver.py
+++ frontend/tests/driver.py
@@ -18,7 +18,7 @@ def create_driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("window-size=2560x1440")
 
-    if os.environ.get("CI_JOB_STAGE") == "selenium_tests":
+    if os.environ.get("CI_JOB_NAME") == "selenium_tests":
         return webdriver.Remote(
             command_executor="http://selenium__standalone-chrome:4444/wd/hub",
             options=chrome_options,
