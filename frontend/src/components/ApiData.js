6095c1ecdea107ea5db827b81239b2d63f39e3ab
jrayyin <77997519+jrayyin@users.noreply.github.com>
2023-03-08 23:13:52 -0600

    all files

---
+++ frontend/src/components/ApiData.js
@@ -0,0 +1,33 @@
+import GitLabImage from "../assets/gitlab.png";
+import YelpImage from "../assets/yelp.jpg";
+import WineImage from "../assets/wine.jpg";
+import TripImage from "../assets/tripadvisor.png";
+
+const ApiData = [
+    {
+      name: "GitLab API",
+      image: GitLabImage,
+      text: "A Javascript library for front-end development",
+      url: "https://docs.gitlab.com/ee/api/",
+    },
+    {
+        name: "Wines API",
+        image: WineImage,
+        text: "A Javascript library for front-end development",
+        url: "https://sampleapis.com/api-list/wines",
+      },
+    {
+        name: "Yelp Fusion API",
+        image: YelpImage,
+        text: "Collaborative software development platform",
+        url: "https://fusion.yelp.com/",
+      },
+      {
+        name: "Tripadvisor API",
+        image: TripImage,
+        text: "Designated communication platform",
+        url: "https://www.tripadvisor.com/developers",
+      },
+    ];
+  
+    export { ApiData };
---
+++ frontend/src/components/DeveloperCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const DeveloperCard = (props) => {
+    console.log(props);
+    const {
+        name,
+        bio,
+        role,
+        commits,
+        issues,
+        unit_tests,
+        image,
+    } = props.devCard
+
+    
+  return (
+    <Card style={{ width: '18rem' , height: '42rem'}}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>
+            {bio}
+            </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+            <ListGroup.Item>{role}</ListGroup.Item>
+            <ListGroup.Item> Commits: {commits}</ListGroup.Item>
+            <ListGroup.Item> Issues: {issues}</ListGroup.Item>
+            <ListGroup.Item> Unit Tests: {unit_tests}</ListGroup.Item>
+        </ListGroup>
+    </Card>
+  );
+}
+
+export default DeveloperCard;
