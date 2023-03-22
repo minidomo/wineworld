a78609b0ef33c2aad9d60e532e474585c1afc333
JB Ladera <domobaby10@gmail.com>
2023-03-22 01:03:56 -0500

    implemented region limits

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from typing import Iterator
 
 from flask import request
-from sqlalchemy.sql.expression import Select
+from sqlalchemy.sql.expression import Select, text
 
 from models import (
     Region,
@@ -39,6 +39,10 @@ db.select()
 
 flask sqlalchemy
 - https://flask-sqlalchemy.palletsprojects.com/en/3.0.x/
+
+converting json property to rows
+- https://vladmihalcea.com/mysql-json-table/
+- https://dev.mysql.com/doc/refman/8.0/en/json-table-functions.html
 """
 
 
@@ -330,6 +334,69 @@ def get_region(id: int):
             "wines": [WineUtil.to_json(e, small=True) for e in wines],
             "vineyards": [VineyardUtil.to_json(e, small=True) for e in vineyards],
         },
+    }
+
+    return data
+
+
+@app.route("/regions/limits", methods=["GET"])
+def get_region_limits():
+    countries_query: Select = db.select(Region.country)
+    countries_query = countries_query.distinct().order_by(Region.country.asc())
+    countries: list[str] = db.session.execute(countries_query).scalars().all()
+
+    trip_type_val = "trip_type_val"
+    trip_types_query = text(
+        f"""
+        SELECT DISTINCT
+            {trip_type_val}
+        FROM
+            {Region.__tablename__},
+            JSON_TABLE(
+                {Region.trip_types.key},
+                '$[*]' COLUMNS(
+                    {trip_type_val} VARCHAR(100) PATH '$'
+                )
+            ) as _temp
+        ORDER BY
+            {trip_type_val}
+            ASC
+    """
+    )
+    trip_types: list[str] = db.session.execute(trip_types_query).scalars().all()
+
+    tag_val = "tag_val"
+    tags_query = text(
+        f"""
+        SELECT DISTINCT
+            {tag_val}
+        FROM
+            {Region.__tablename__},
+            JSON_TABLE(
+                {Region.tags.key},
+                '$[*]' COLUMNS(
+                    {tag_val} VARCHAR(100) PATH '$'
+                )
+            ) as _temp
+        ORDER BY
+            {tag_val}
+            ASC
+    """
+    )
+    tags: list[str] = db.session.execute(tags_query).scalars().all()
+
+    data = {
+        "rating": {
+            "min": 0.0,
+            "max": 5.0,
+        },
+        "reviews": {
+            "min": 0,
+        },
+        "tripTypes": trip_types,
+        "tags": tags,
+        "countries": countries,
+        "sort": [],
     }
 
     return data
