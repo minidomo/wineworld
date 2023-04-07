5c89a4484316bbb194e9dac58c08f90ab6bf7205
austin25coding <acbarret@outlook.com>
2023-04-07 00:45:25 -0500

    vineyards bar graph implemented

--- frontend/src/containers/Visualizations.js
+++ frontend/src/containers/Visualizations.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Visualizations = () => <div>Visualizations</div>;
+import BarGraph from '../components/BarGraph';
+
+const Visualizations = () => (
+  <div>
+    Visualizations
+    <BarGraph />
+  </div>
+);
 
 export default Visualizations;
