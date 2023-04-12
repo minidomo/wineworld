f14424ca295d1c48978ad8e81a3c97aae428c531
rparappuram <rparappuram@gmail.com>
2023-04-12 18:52:26 -0500

    validated U.S. state names for choropleth

--- backend/src/routes/visualizations/provider/choropleth/index.py
+++ backend/src/routes/visualizations/provider/choropleth/index.py
@@ -2,7 +2,11 @@ import requests
 from flask_restful import Resource
 from typing import Any
 
-from src.util.general import JsonObject
+# from src.util.general import JsonObject
+
+JsonObject = dict[str, Any]
+
+state_names=["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
 def get_response() -> JsonObject:
     return requests.get("https://api4.parkscape.me/airports").json()
@@ -11,10 +15,11 @@ def count_airports(response: JsonObject) -> JsonObject:
     states:JsonObject = {}
     for airport in response["data"]:
         state = airport["state"]
-        if state in states:
-            states[state] += 1
-        else:
-            states[state] = 1
+        if state in state_names:
+            if state in states:
+                states[state] += 1
+            else:
+                states[state] = 1
     return states
 
 def create_response(states: JsonObject) -> JsonObject:
@@ -36,3 +41,6 @@ class VisualizationChoropleth(Resource):
         states = count_airports(response)
         data = create_response(states)
         return data
+    
+a = VisualizationChoropleth()
+print(a.get())
