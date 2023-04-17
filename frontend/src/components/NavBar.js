bf5e8dc8f148505ea74ceb5c9fed188f0fc4d720
austin25coding <acbarret@outlook.com>
2023-04-16 21:02:16 -0500

    navbar has old layout again

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -15,58 +15,57 @@ const NavBar = () => {
     event.preventDefault();
     navigate(`/search/${query}`);
   };
-
   return (
-    <Navbar variant='navbar fixed-top' expand='lg'>
-      {/* <Navbar className="custom fixed-top navbar-expand-lg"> */}
-      <div class="container-fluid">
-        <Link class="navbar-brand" to="/">
-          WineWorld
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <Link class="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/wines">
-                Wines
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/vineyards">
-                Vineyards
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/regions">
-                Regions
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/visualizations">
-                Visualizations
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/provider">
-                Provider Visualizations
-              </Link>
-            </li>
-            <li class="nav-item">
-              {DarkMode('toggle')}
-            </li>
-          </Nav>
-          <Form onSubmit={handleSubmit} className="d-flex">
-            <Form.Control className="custom" type="search" placeholder="search"
-              onChange={event => setQuery(event.target.value)} />
-          </Form>
-        </Navbar.Collapse>
-      </div>
-      {/* </Navbar> */}
+    <Navbar variant={`navbar bg-${DarkMode()}`} expand='lg' data-bs-theme={DarkMode()}>
+      <Navbar variant="custom fixed-top bg-body-tertiary" expand='lg'>
+        <div class="container-fluid">
+          <Link class="navbar-brand" to="/">
+            WineWorld
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse>
+            <Nav className="navbar-nav me-auto mb-2 mb-lg-0">
+              <Nav.Item>
+                <Link class="nav-link" to="/about">
+                  About
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link class="nav-link" to="/wines">
+                  Wines
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link class="nav-link" to="/vineyards">
+                  Vineyards
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link class="nav-link" to="/regions">
+                  Regions
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link class="nav-link" to="/visualizations">
+                  Visualizations
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link class="nav-link" to="/provider">
+                  Provider Visualizations
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                {DarkMode('toggle')}
+              </Nav.Item>
+            </Nav>
+            <Form onSubmit={handleSubmit} className="d-flex">
+              <Form.Control className="custom" type="search" placeholder="search"
+                onChange={event => setQuery(event.target.value)} />
+            </Form>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
     </Navbar>
   );
 };
