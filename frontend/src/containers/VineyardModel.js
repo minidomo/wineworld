87e45ad183f1625363886885956cf9bfab912033
JB Ladera <domobaby10@gmail.com>
2023-04-18 02:14:39 -0500

    merge and format

--- frontend/src/containers/VineyardModel.js
+++ frontend/src/containers/VineyardModel.js
@@ -179,8 +179,7 @@ const VineyardModel = () => {
           </Dropdown>
         </Col>
         <Col>
-          <SearchBar placeholder="Search vineyards" setValue={setSearchQuery}
-            valueIfEmpty={undefined} />
+          <SearchBar placeholder="Search vineyards" setValue={setSearchQuery} valueIfEmpty={undefined} />
         </Col>
       </Row>
       <br />
