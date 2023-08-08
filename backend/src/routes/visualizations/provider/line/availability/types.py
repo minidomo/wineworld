a6b5bfa38e1a313df735475824e0710c31d9b1c3
JB Ladera <minidomou@gmail.com>
2023-08-08 03:07:12 -0500

    initial typescript backend

--- backend/src/routes/visualizations/provider/line/availability/types.py
+++
@@ -1,7 +0,0 @@
-from typing import Callable, Optional
-
-import pendulum
-
-Availability = tuple[Optional[pendulum.Time], Optional[pendulum.Time]]
-TimeMatchFunction = Callable[[str], bool]
-TimeConversionFunction = Callable[[str], Availability]
--- backend/tests/schemas/coordinates.py
+++
@@ -1,9 +0,0 @@
-coordinates_schema = {
-    "type": "object",
-    "properties": {
-        "latitude": {"type": "number"},
-        "longitude": {"type": "number"},
-    },
-    "required": ["latitude", "longitude"],
-    "additionalProperties": False,
-}
