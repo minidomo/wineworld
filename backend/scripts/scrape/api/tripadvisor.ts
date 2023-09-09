370b279cee9643f92ba58f5763dd267fa072241d
JB Ladera <domobaby10@gmail.com>
2023-09-09 00:12:47 -0500

    added tripadvisor api functions

--- backend/scripts/scrape/api/tripadvisor.ts
+++ backend/scripts/scrape/api/tripadvisor.ts
@@ -10,8 +10,8 @@ export interface AddressObject {
     postalcode?: string,
     address_string?: string,
     phone?: string,
-    latitude?: number,
-    longitude?: number,
+    latitude?: string,
+    longitude?: string,
 }
 
 export interface SimpleLocationObject {
@@ -31,8 +31,103 @@ export interface ErrorResponse {
     error: {
         message: string,
         type: string,
-        code: number,
+        code: string,
     },
+}
+
+export interface AncestorObject {
+    abbrv?: string,
+    level: string,
+    name: string,
+    location_id: string,
+}
+
+export interface RankingDataObject {
+    geo_location_id: string,
+    ranking_string: string,
+    geo_location_name: string,
+    ranking_out_of: string,
+    ranking: string,
+}
+
+export interface ReviewRatingCountObject {
+    '1': string,
+    '2': string,
+    '3': string,
+    '4': string,
+    '5': string,
+}
+
+export interface PeriodObject {
+    open: {
+        day: number,
+        time: string,
+    },
+    close: {
+        day: number,
+        time: string,
+    },
+}
+
+export interface CategoryObject {
+    name: string,
+    localized_name: string,
+}
+
+export interface HoursObject {
+    periods: PeriodObject[],
+    weekday_text: string[],
+    subcategory?: CategoryObject[],
+}
+
+export interface GroupObject {
+    name: string,
+    localized_name: string,
+    categories: CategoryObject[],
+}
+
+export interface TripTypeObject {
+    name: string,
+    localized_name: string,
+    value: string,
+}
+
+export interface LocationDetailsResponse {
+    location_id: string,
+    name: string,
+    description: string,
+    web_url: string,
+    address_obj: AddressObject,
+    ancestors: AncestorObject[],
+    latitude?: string,
+    longitude?: string,
+    timezone: string,
+    email?: string,
+    phone?: string,
+    website?: string,
+    write_review: string,
+    ranking_data: RankingDataObject,
+    rating: string,
+    rating_image_url: string,
+    num_reviews: string,
+    review_rating_count: ReviewRatingCountObject,
+    subratings?: any,
+    photo_count: string,
+    see_all_photos: string,
+    price_level: string,
+    hours: HoursObject,
+    amenities?: string[],
+    features?: string[],
+    cuisine?: any[],
+    parent_brand?: string,
+    brand?: string,
+    category: CategoryObject,
+    subcategory: CategoryObject[],
+    groups: GroupObject[],
+    styles?: string[],
+    neighborhood_info?: any[],
+    trip_types: TripTypeObject[],
+    awards: any[],
 }
 
 // params - https://tripadvisor-content-api.readme.io/reference/searchfornearbylocations
@@ -46,4 +141,16 @@ export async function nearbyLocationSearch(key: string, params: Record<string, s
         }
     });
     return await response.json() as NearbyLocationSearchResponse | ErrorResponse;
+}
+
+export async function locationDetails(key: string, locationId: string, params: Record<string, string | readonly string[]>) {
+    const baseUrl = `https://api.content.tripadvisor.com/api/v1/location/${locationId}/details`;
+    const url = `${baseUrl}?` + new URLSearchParams({ ...params, key });
+    const response = await fetch(url, {
+        method: 'get',
+        headers: {
+            'accept': 'application/json',
+        }
+    });
+    return await response.json() as LocationDetailsResponse | ErrorResponse;
 }
